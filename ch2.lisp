a2010b5a69657a2c64ef1ae186892c9fad880914
Patrick Robotham <patrick.robotham2@gmail.com>
2017-12-30 00:29:51 +1100

    Added exercises six, seven, eight.

--- ch2.lisp
+++ ch2.lisp
@@ -60,3 +60,65 @@
 ;; mystery finds the position where the first instance of x occurs in y.
 ;; If x does not occur in y, mystery returns nil.
 (mystery 3 `(1 3 3)) ;; returns 1
+
+;; EXERCISE SIX:
+;; What could occur in place of the x in each of the following exchanges?
+;; (a)
+(car (x cdr `(a (b c) d))) ;; returns b
+
+;; (b)
+(x 13 (/ 1 0)) ;; returns 13
+
+;; (c)
+(x #'list 1 nil) ;; returns (1)
+
+;; (a) car, (b) or, (c) apply
+
+;; EXERCISE SEVEN:
+;; Using only operators introduced in this chapter, define a function that
+;; takes a list as an argument and returns true if one of its elements is a
+;; list.
+
+(defun has-list (lst)
+  (if (null lst)
+      nil
+      (if (listp (car lst))
+          t
+          (has-list (cdr lst)))))
+
+(has-list '(1 (2 3) 4)) ;; returns t
+(has-list '(1 2)) ;; returns nil
+
+;; EXERCISE EIGHT:
+;; Give iterative and recursive definitions of a function that
+;; (a) takes a positive integer and prints that many dots.
+;; (b) takes a list and returns the number of times the symbol 'a' occurs in it.
+
+;; (a) iterative
+(defun ndots (n)
+  (do ((i 1 (+ i 1)))
+      ((> i n) (format t "~%"))
+      (format t ".")))
+;; (a) recursive
+(defun ndots (n)
+  (if (<= n 0)
+      (format t "~%")
+      (or (format t ".")
+          (ndots (- n 1)))))
+
+;; (b) iterative
+(defun count-a (lst)
+  (let ((len 0))
+    (dolist (obj lst)
+      (if (eql obj 'a)
+          (setf len (+ len 1))
+        nil))
+    len))
+;; (b) recursive
+(defun count-a (lst)
+  (if (null lst)
+      0
+      (if (eql 'a (car lst))
+          (let ((count (count-a (cdr lst))))
+            (+ count 1))
+        0)))
